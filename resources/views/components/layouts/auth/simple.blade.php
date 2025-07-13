e6ad4b899d9f032cae1329b6eb21c1ae07f675e7
Icecoldl334 <fauzanimam334@gmail.com>
2025-07-13 20:26:24 +0700

    push view login

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-    <head>
-        @include('partials.head')
-    </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div class="flex w-full max-w-sm flex-col gap-2">
-                <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
-                    </span>
-                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
-                </a>
-                <div class="flex flex-col gap-6">
-                    {{ $slot }}
-                </div>
+
+<head>
+    @include('partials.head')
+</head>
+
+<body class="min-h-screen bg-gradient-to-b from-primary-500 to-primary-600 antialiased dark:bg-linear-to-b ">
+    <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div
+            class="flex w-full p-8 bg-gradient-to-b from-primary-600 to-primary-700 rounded-lg shadow-2xl max-w-sm flex-col gap-2">
+            <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
+                <span class="flex h-40 w-40 mb-1 items-center justify-center rounded-md">
+                    <x-app-logo-icon class="size-9 fill-current text-black " />
+                </span>
+                <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+            </a>
+            <div class="flex flex-col gap-6">
+                {{ $slot }}
             </div>
         </div>
-        @fluxScripts
-    </body>
+    </div>
+    @fluxScripts
+</body>
+
 </html>
