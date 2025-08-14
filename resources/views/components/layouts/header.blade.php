51ac97ee6831d1a447139d0ef2a26c9ad8194b23
Icecold334 <fauzanimam334@gmail.com>
2025-08-14 15:58:11 +0700

    update bug icon

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -2,7 +2,9 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Kedai</title>
-  <script src="https://kit.fontawesome.com/5fd2369345.js" crossorigin="anonymous">
+  {{-- <script src="https://kit.fontawesome.com/5fd2369345.js" crossorigin="anonymous">
+    --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
 
   </script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
